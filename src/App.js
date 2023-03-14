8ed82be709d5fadcb8e89cc43683f4e2f3c341d7
Ram-tyagi <ramtyagi04@gmail.com>
2023-03-15 00:32:05 +0530

    app

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ const App=()=> {
 
 const addExpenseHandler=(expense)=>{
   setExpenses ((prevExpenses)=>{
-    return [expense,...prevExpenses]
+    return [expense, ...prevExpenses]
   })
   
    
